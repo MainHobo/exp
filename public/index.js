4ca9703ca57e4dd959416dccad59af0083c912cb
Anders <anderskvalo@outlook.com>
2023-03-31 16:00:02 +0200

    The server now listens for requests from the frontend

--- public/index.js
+++ public/index.js
@@ -7,6 +7,15 @@ function helloExpress() {
       const lon = position.coords.longitude;
       document.getElementById("latitude").textContent = lat;
       document.getElementById("longitude").textContent = lon;
+
+      const data = { lat, lon };
+      fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
     });
   } else {
     console.log("geolocation not available");
--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const app = express();
 const port = 3000;
 app.listen(process.env.PORT || port);
 app.use(express.static("public"));
+
+app.post("/api", (req, res) => {
+  console.log(req.body);
+});
