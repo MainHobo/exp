413d528f0b20af7325a698a621a0274e8037dcb3
Anders <anderskvalo@outlook.com>
2023-03-31 13:35:08 +0200

    Testing if geolocation is available, and printing your possition in console

--- public/index.js
+++ public/index.js
@@ -1,8 +1,17 @@
 helloExpress();
 function helloExpress() {
+  if ("geolocation" in navigator) {
+    console.log("geolocation available");
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position);
+    });
+  } else {
+    console.log("geolocation not available");
+  }
+
   let html = "";
   html += `
-        <h1>Hello Express</h1>
+        <h1>Geolocation</h1>
     `;
   document.getElementById("app").innerHTML = html;
 }
