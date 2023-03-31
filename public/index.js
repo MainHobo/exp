62aa852fbc5f32ab334e872d4b7437909e9faa4e
Anders <anderskvalo@outlook.com>
2023-03-31 14:06:53 +0200

    Presenting the latitude and longitude of your position on the screen

--- public/index.js
+++ public/index.js
@@ -3,15 +3,20 @@ function helloExpress() {
   if ("geolocation" in navigator) {
     console.log("geolocation available");
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      document.getElementById("latitude").textContent = lat;
+      document.getElementById("longitude").textContent = lon;
     });
   } else {
     console.log("geolocation not available");
   }
-
   let html = "";
   html += `
-        <h1>Geolocation</h1>
-    `;
+    <p>
+      Latitude: <span id="latitude"></span>&deg; <br />
+      Longitude: <span id="longitude"></span>&deg;
+    </p>
+  `;
   document.getElementById("app").innerHTML = html;
 }
